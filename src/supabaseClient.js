e48678d8660e23da8ea638ee92ff545d2023b71b
TeoYK <65156368+yk1502@users.noreply.github.com>
2025-12-29 13:20:10 +0800

    Update api keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.PUBLIC_URL;
-const supabaseAnonKey = process.env.ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_PUBLIC_URL;
+const supabaseAnonKey = process.env.REACT_APP_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
